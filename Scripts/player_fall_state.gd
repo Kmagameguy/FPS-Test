15b15736b23cca27bbbd50303ea759e89f4230a2
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-08-10 16:17:27 -0400

    Godot 4.7 & Bug Fixes (#2)
    * Update to Godot 4.7
    
    * Inverse update & physics_update Code; Woops
    
    * Fix Jump-Related Bug When Tip-Toeing
    
    * Fix Jump Floatiness
    
    * Add *.uid Files
    
    * Clear Transformation Warning

--- Scripts/player_fall_state.gd
+++ Scripts/player_fall_state.gd
@@ -10,18 +10,21 @@ func enter(previous_state: PlayerState) -> void:
 	_previous_state = previous_state
 
 func update(delta: float) -> void:
+	PLAYER.update_fov(SPEED, delta)
+
+func physics_update(delta: float) -> void:
 	PLAYER.update_gravity(delta)
 	PLAYER.update_input(SPEED, ACCELERATION, DECELERATION)
 	PLAYER.update_velocity()
 
-	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION) && _not_previously_jumping():
-		transition.emit(PLAYER.STATES.JUMP.NAME)
+	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION):
+		if _not_previously_jumping():
+			transition.emit(PLAYER.STATES.JUMP.NAME)
+		elif _previous_state is PlayerJumpState:
+			transition.emit(PLAYER.STATES.DOUBLE_JUMP.NAME)
 
 	if PLAYER.is_on_floor():
 		transition.emit(PLAYER.STATES.IDLE.NAME)
-
-func update_physics(delta: float) -> void:
-	PLAYER.update_fov(SPEED, delta)
 
 func _not_previously_jumping() -> bool:
 	return !(_previous_state is PlayerJumpState ||
--- Scripts/player_tip_toe_state.gd
+++ Scripts/player_tip_toe_state.gd
@@ -5,6 +5,10 @@ class_name PlayerTipToeState extends PlayerMovementState
 @export var DECELERATION: float = 0.25
 
 func update(delta: float) -> void:
+	PLAYER.update_headbob(delta)
+	PLAYER.update_fov(SPEED, delta)
+
+func physics_update(delta: float) -> void:
 	PLAYER.update_gravity(delta)
 	PLAYER.update_input(SPEED, ACCELERATION, DECELERATION)
 	PLAYER.update_velocity()
@@ -12,7 +16,7 @@ func update(delta: float) -> void:
 	if Input.is_action_just_released(PLAYER.STATES.TIP_TOE.ACTION):
 		transition.emit(PLAYER.STATES.WALK.NAME)
 
-	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION):
+	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION) && PLAYER.is_on_floor():
 		transition.emit(PLAYER.STATES.JUMP.NAME)
 
 	if PLAYER.velocity.length() == 0.0 && PLAYER.is_on_floor():
@@ -20,7 +24,3 @@ func update(delta: float) -> void:
 
 	if PLAYER.velocity.y < -3.0 && PLAYER.is_in_air():
 		transition.emit(PLAYER.STATES.FALL.NAME)
-
-func update_physics(delta: float) -> void:
-	PLAYER.update_headbob(delta)
-	PLAYER.update_fov(SPEED, delta)
